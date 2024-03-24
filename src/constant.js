df47233f4bf381686fe13b442a750ac8e47c5663
Özhanbirgül <birgulozhan9@gmail.com>
2024-03-24 14:48:26 +0300

    first commit

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const options = {
+    method: 'GET',
+    url: 'https://text-translator2.p.rapidapi.com/getLanguages',
+    headers: {
+      'X-RapidAPI-Key': 'd9d98d4890msh1d7994cd5c078c8p1dec60jsn75f913f0915e',
+      'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+    }
+  };
